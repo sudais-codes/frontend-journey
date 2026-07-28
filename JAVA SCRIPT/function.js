6ad91b80ff000a0a412e0976e8bdb12b14061273
sudais-codes <sudaissarai130@gmail.com>
2026-07-28 14:50:36 -0700

    JS

---
+++ JAVA SCRIPT/function.js
@@ -0,0 +1,18 @@
+function print(){
+    "HELLO THIS IS SUDAIS";
+}
+
+console.log(print);
+
+function add(a , b){
+    let sum = a+b;
+    return sum;
+}
+
+let sum = add(5 , 10);
+console.log(sum);
+
+function fruit(){
+    "APPLE BANANA";
+}
+console.log(fruit);
---
+++ JAVA SCRIPT/practice0.js
@@ -0,0 +1,35 @@
+console.log("HELLO THIS IS SUDAIS KHAN");
+console.log(38);
+console.log(3+4);
+console.log((3+3) * 7);
+console.log(0/0);
+
+console.log("Hey this a strig with double quotes");
+console.log('Hey this a string with single qoute');
+console.log("This is an addition of multi strings");
+console.log("Cat " + "Is" + "  " + "Moving");
+console.log("Using leterals to print some text and numbers");
+let name= "Sudais";
+console.log(`Hey my name is ${name + " khan"} and im ${20} years old`);
+
+console.log("let see unary operator");
+let a =10;
+let b = --a;
+console.log(b);
+
+let c = 10;
+let d = ++c;
+console.log(d);
+
+console.log("Bolean operator");
+console.log(3 < 4);
+console.log(3 > 4);
+
+console.log("Ternary operator")
+
+let mood = "happy";
+console.log(mood == "happy" ? "😊" : "😞");
+
+let x;
+console.log(x);
+
